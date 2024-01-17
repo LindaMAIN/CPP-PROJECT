a9da6c9bf5e7860b3a04ae88eaffbc927e470e0d
ClemZh <clement.zhuang@etu.sorbonne-universite.fr>
2024-01-17 11:22:24 +0100

    display image

--- Olympic_sim/mainwindow.cpp
+++ Olympic_sim/mainwindow.cpp
@@ -1,44 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-//export PATH=/home/zclem/Qt/6.6.1/gcc_64/bin:$PATH
-
 #include <QPixmap>
+#include <QLabel>
 #include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     mainStackedWidget = ui->mainStackedWidget;
 
-    //MAP display
+    // MAP display
     QPixmap pix(QDir::currentPath() + "/images/paris_map.png");
-    qDebug() << QDir::currentPath() + "/images/paris_map.png";
 
     if (pix.isNull()) {
-    qDebug() << "Failed to load image file";
-    } 
-    else {
-        QLabel *mapLabel = new QLabel(this); // Create a new QLabel
-        mapLabel->setPixmap(pix); // Set the QPixmap to the QLabel
-        ui->verticalLayout_map->addWidget(mapLabel); // Add the QLabel to your layout
+        qDebug() << "Failed to load image file";
+    } else {
+        QLabel *mapLabel = new QLabel(this);
+
+        // Set the aspect ratio policy to keep the image's aspect ratio
+        mapLabel->setScaledContents(true);
+
+        // Set the alignment to center the image
+        mapLabel->setAlignment(Qt::AlignCenter);
+
+        // Set the pixmap to the label
+        mapLabel->setPixmap(pix);
+
+        // Add the QLabel to your layout
+        ui->verticalLayout_map->addWidget(mapLabel);
     }
 
-    //Create an action with menu
-    QAction* accueilAction = ui->menuMenu->addAction("Accueil Page");
-    QAction* settingsAction = ui->menuMenu->addAction("Simulation Settings Page");
-    QAction* analyseAction = ui->menuMenu->addAction("Analyse Page");
-    QAction* overviewAction = ui->menuMenu->addAction("Overview Page");
+    // Create an action with the menu
+    QAction *accueilAction = ui->menuMenu->addAction("Accueil Page");
+    QAction *settingsAction = ui->menuMenu->addAction("Simulation Settings Page");
+    QAction *analyseAction = ui->menuMenu->addAction("Analyse Page");
+    QAction *overviewAction = ui->menuMenu->addAction("Overview Page");
 
     connect(accueilAction, &QAction::triggered, this, &MainWindow::showFirstPage);
     connect(settingsAction, &QAction::triggered, this, &MainWindow::showMainPage);
     connect(analyseAction, &QAction::triggered, this, &MainWindow::showAnalysePage);
     connect(overviewAction, &QAction::triggered, this, &MainWindow::showOverviewPage);
-
-//////
-    
 }
 
 MainWindow::~MainWindow()
@@ -46,16 +49,23 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-//Index set to alphabetical order
-void MainWindow::showFirstPage(){
+// Index set to alphabetical order
+void MainWindow::showFirstPage()
+{
     mainStackedWidget->setCurrentIndex(0);
 }
-void MainWindow::showOverviewPage(){
+
+void MainWindow::showOverviewPage()
+{
     mainStackedWidget->setCurrentIndex(1);
 }
-void MainWindow::showAnalysePage(){
+
+void MainWindow::showAnalysePage()
+{
     mainStackedWidget->setCurrentIndex(2);
 }
-void MainWindow::showMainPage(){
+
+void MainWindow::showMainPage()
+{
     mainStackedWidget->setCurrentIndex(3);
 }

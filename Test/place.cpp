9363de454d8c864d3a30345bdb7b2692bc25c403
Zclem <112188124+Zclem@users.noreply.github.com>
2024-02-08 09:32:51 +0100

    Delete Test directory

--- Test/place.cpp
+++
@@ -1,9 +0,0 @@
-#include "place.h"
-
-int Place::getCurrentNumber() const {
-    return number;
-}
-
-int Place::getMaxCapacity() const {
-    return max_capacity;
-}
--- Test/truc.h
+++
@@ -1,4 +0,0 @@
-#ifndef TRUC_H
-#define TRUC_H
-
-#endif // TRUC_H
